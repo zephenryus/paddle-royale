f91760f8c005fdd8072bdd804c2feb544bee0046
Kevin Jensen <kevin@turtlerockweb.com>
2025-05-19 21:38:36 -0600

    Added paddle collisions, scoring, and sound effects.

--- src/ball.py
+++ src/ball.py
@@ -10,22 +10,30 @@ class Ball:
         self.speed_y = random.choice([-1, 1]) * speed
         self.radius = radius
 
-    def check_collision(self, paddle_rects):
+    def check_collision(self, paddle_rects, bump_sound=None):
         for paddle in paddle_rects:
             if self.rect.colliderect(paddle):
                 self.speed_x *= -1
+
+                if bump_sound:
+                    bump_sound.play()
                 break
 
-    def update(self, screen_width, screen_height, paddle_rects):
+    def update(self, screen_width, screen_height, paddle_rects, bump_sound=None):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
         if self.rect.top <= 0 or self.rect.bottom >= screen_height:
             self.speed_y *= -1
 
-        self.check_collision(paddle_rects)
+        self.check_collision(paddle_rects, bump_sound)
 
         # TODO: Add scoring
 
     def draw(self, surface):
         pygame.draw.ellipse(surface, (255, 255, 255), self.rect)
+
+    def reset(self, x, y):
+        self.rect.center = (x, y)
+        self.speed_x *= -1
+        self.speed_y = random.choice([-1, 1]) * abs(self.speed_y)
--- src/main.py
+++ src/main.py
@@ -17,13 +17,22 @@ WHITE = (255, 255, 255)
 def main():
     # Initialize PyGame
     pygame.init()
+    if not pygame.mixer.get_init():
+        pygame.mixer.init()
+
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Paddle Royale")
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(30, (WINDOW_HEIGHT - 100) // 2)
-    right_paddle = Paddle(WINDOW_WIDTH - 40, (WINDOW_HEIGHT - 100) // 2)
+    right_paddle = Paddle(WINDOW_WIDTH - 40, (WINDOW_HEIGHT - 100) // 2, speed=10)
     ball = Ball(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+    left_score = 0
+    right_score = 0
+
+    font = pygame.font.SysFont(None, 64)
+    paddle_bump_sound = pygame.mixer.Sound("sounds/paddle_bump.wav")
+    goal_sound = pygame.mixer.Sound("sounds/goal.wav")
 
     # Main game loop
     running = True
@@ -36,7 +45,17 @@ def main():
         # TODO: Add game logic
         left_paddle.move(pygame.K_w, pygame.K_s, WINDOW_HEIGHT)
         right_paddle.move(pygame.K_UP, pygame.K_DOWN, WINDOW_HEIGHT)
-        ball.update(WINDOW_WIDTH, WINDOW_HEIGHT, [left_paddle.rect, right_paddle.rect])
+        ball.update(WINDOW_WIDTH, WINDOW_HEIGHT, [left_paddle.rect, right_paddle.rect], paddle_bump_sound)
+
+        # Check for scoring
+        if ball.rect.left <= 0:
+            right_score += 1
+            goal_sound.play()
+            ball.reset(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+        elif ball.rect.right >= WINDOW_WIDTH:
+            left_score += 1
+            goal_sound.play()
+            ball.reset(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
 
         # Drawing
         screen.fill(BLACK)
@@ -45,6 +64,12 @@ def main():
         left_paddle.draw(screen)
         right_paddle.draw(screen)
         ball.draw(screen)
+
+        # Draw score
+        left_text = font.render(str(left_score), True, WHITE)
+        right_text = font.render(str(right_score), True, WHITE)
+        screen.blit(left_text, (WINDOW_WIDTH // 4 - left_text.get_width() // 2, 20))
+        screen.blit(right_text, (3 * WINDOW_WIDTH // 4 - right_text.get_width() // 2, 20))
 
         pygame.display.flip()
         clock.tick(FPS)
